4ffc5d0776d21a0028074e619a330b60a47abbcc
Datta <2096688@cognizant.com>
2022-05-05 20:19:55 +0530

    basic Link example

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import Link from 'next/Link'
 
 function NewsPage () {
   return (
-    <div> NewsPage </div>
+    <div> 
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/1'> 1st Article
+          </Link>
+        </li>
+        <li>
+          <Link href='/news/2'> 2nd Article
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
